0257886a953cb27f36c7d86f7f50b8e15a448eff
Milancho Arsovski <milancho.arsovski@gmail.com>
2024-11-23 09:03:20 +0100

    Added TFAGoogleAuthenticatorAngular

---
+++ TFAGoogleAuthenticatorAngular/TFAGoogleAuthenticatorAngular/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TwoFactorAuthenticationGoogleAuthenticatorAngular.DataTransferObjects;
+
+namespace TFAGoogleAuthenticatorAngular.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public Task<IActionResult> Get()
+        {
+            return Task.FromResult<IActionResult>(Ok());
+        }
+    }
+}
