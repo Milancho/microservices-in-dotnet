eef3083bae8562a890c56d72a0ffe964f5dafc2e
Milancho Arsovski <milancho.arsovski@gmail.com>
2024-12-01 08:55:05 +0100

    Extending our E-Commerce Application

--- order-microservice/Order.Service/Program.cs
+++ order-microservice/Order.Service/Program.cs
@@ -1,10 +1,15 @@
+using Order.Service.Endpoints;
 using Order.Service.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
 builder.Services.AddScoped<IOrderStore, InMemoryOrderStore>();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.RegisterEndpoints();
+
 app.Run();
+
