e859513195b992d4545f3b3a003309fa11b72093
Milancho Arsovski <milancho.arsovski@gmail.com>
2025-07-20 16:52:49 +0200

    UseOpenTelemetryPrometheusScrapingEndpoint

--- shared-libs/ECommerce.Shared/Observability/OpenTelemetryStartupExtensions.cs
+++ shared-libs/ECommerce.Shared/Observability/OpenTelemetryStartupExtensions.cs
@@ -1,5 +1,6 @@
 using ECommerce.Shared.Infrastructure.RabbitMq;
 using ECommerce.Shared.Observability.Metrics;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OpenTelemetry;
@@ -37,21 +38,26 @@ public static class OpenTelemetryStartupExtensions
     public static TracerProviderBuilder WithSqlInstrumentation(this TracerProviderBuilder builder) =>
         builder.AddSqlClientInstrumentation();
 
-    public static OpenTelemetryBuilder AddOpenTelemetryMetrics(this OpenTelemetryBuilder openTelemetryBuilder, 
+    public static OpenTelemetryBuilder AddOpenTelemetryMetrics(this OpenTelemetryBuilder openTelemetryBuilder,
     string serviceName, IServiceCollection services,
     Action<MeterProviderBuilder>? customMetrics = null)
-{
-    services.AddSingleton(new MetricFactory(serviceName));
+    {
+        services.AddSingleton(new MetricFactory(serviceName));
 
-    return openTelemetryBuilder
-        .WithMetrics(builder =>
-        {
-            builder
-                .AddConsoleExporter()
-                .AddAspNetCoreInstrumentation()
-                .AddMeter(serviceName);
+        return openTelemetryBuilder
+            .WithMetrics(builder =>
+            {
+                builder
+                    .AddConsoleExporter()
+                    .AddAspNetCoreInstrumentation()
+                    .AddMeter(serviceName)
+                    .AddPrometheusExporter();
 
-            customMetrics?.Invoke(builder);
-        });
-}
+                customMetrics?.Invoke(builder);
+            });
+    }
+
+    public static void UsePrometheusExporter(this WebApplication webApplication) =>
+           webApplication.UseOpenTelemetryPrometheusScrapingEndpoint();
+        
 }
