7e34e141fa7a0dc40723c51de1ed77eabcd208ba
Milancho Arsovski <milancho.arsovski@gmail.com>
2025-07-24 15:15:53 +0200

    Outbox DbContext

---
+++ shared-libs/ECommerce.Shared/Infrastructure/Outbox/OutboxOptions.cs
@@ -0,0 +1,8 @@
+namespace ECommerce.Shared.Infrastructure.Outbox;
+public class OutboxOptions
+{
+    public const string OutboxSectionName = "Outbox";
+
+    public int PublishIntervalInSeconds { get; set; } = 30;
+}
+
