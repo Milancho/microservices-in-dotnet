7fd905c0c0575711c6676d4b975204108a0863d6
Milancho Arsovski <milancho.arsovski@gmail.com>
2025-07-28 17:20:34 +0200

    Store User Information

--- auth-microservice/Auth.Service/Program.cs
+++ auth-microservice/Auth.Service/Program.cs
@@ -1,6 +1,20 @@
+using Auth.Service.Infrastructure.Data.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSqlServerDatastore(builder.Configuration);
+
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.MigrateDatabase();
+}
+
+// app.RegisterEndpoints();
+
+app.UseHttpsRedirection();
 
 app.Run();
