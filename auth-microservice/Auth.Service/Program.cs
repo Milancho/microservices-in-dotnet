46b7ffc1f9f1b6bf6ab3c4178d9b68a6d381aab9
Milancho Arsovski <milancho.arsovski@gmail.com>
2025-07-29 11:49:21 +0200

    Generate JWTs

--- auth-microservice/Auth.Service/Program.cs
+++ auth-microservice/Auth.Service/Program.cs
@@ -1,9 +1,11 @@
 using Auth.Service.Infrastructure.Data.EntityFramework;
+using Auth.Service.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSqlServerDatastore(builder.Configuration);
 
+builder.Services.RegisterTokenService(builder.Configuration);
 
 
 var app = builder.Build();
